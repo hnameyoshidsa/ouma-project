c6f5bdd7eecab8486fec076473069af2061b871a
haya-keiba <61572820+haya-keiba@users.noreply.github.com>
2020-03-10 03:29:46 +0900

    URLを結合するプログラム

---
+++ make_url.py
@@ -0,0 +1,9 @@
+file_name = "raceid.txt" #レースIDファイル名
+write_name = "_url.txt" #出力先のファイル名
+base_url = "https:" #結合するURL(共通部分)
+
+with open(write_name, mode="w") as fw:
+    with open(file_name, mode="r") as f:
+        ids = f.read().split("\n")
+        for id in ids:
+            fw.write(base_url + id + "\n") #書き込み
