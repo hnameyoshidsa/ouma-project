eed68faa6570db12d10a35a4e65ee91604d6be63
haya-keiba <61572820+haya-keiba@users.noreply.github.com>
2020-03-10 04:04:17 +0900

    url added

--- make_url.py
+++ make_url.py
@@ -1,6 +1,6 @@
 file_name = "raceid.txt" #レースIDファイル名
 write_name = "_url.txt" #出力先のファイル名
-base_url = "https:" #結合するURL(共通部分)
+base_url = "https://db.netkeiba.com/race/" #結合するURL(共通部分)
 
 with open(write_name, mode="w") as fw:
     with open(file_name, mode="r") as f:
